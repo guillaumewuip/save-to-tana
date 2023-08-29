7d6389b7cbf2b99c9e352bd7c62af2013e65d4c4
Guillaume Clochard <guillaume.clochard@blablacar.com>
2023-08-29 22:13:14 +0200

    trash redis hash

--- rss-to-tana/store.js
+++ rss-to-tana/store.js
@@ -12,7 +12,7 @@ client.on('reconnecting', () => Log.debug('Redis Client reconnecting...'));
 client.on('ready', () => Log.info('Redis Client Ready!'));
 
 // change the prefix to trash all existing ids
-const storeId = (id) => `1-${id}`
+const storeId = (id) => `2-${id}`
 
 const initialize = async () => {
   Log.info('Connecting to Redis', REDIS_URL)
